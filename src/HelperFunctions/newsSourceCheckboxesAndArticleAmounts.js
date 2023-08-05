34b0c0b45a8b488e395d9089daf621adc9c028a8
Nick <nickhoefle@gmail.com>
2023-08-05 16:04:59 -0500

    Plus/Minus article amount buttons implemented for mobile.

--- src/HelperFunctions/newsSourceCheckboxesAndArticleAmounts.js
+++ src/HelperFunctions/newsSourceCheckboxesAndArticleAmounts.js
@@ -7,6 +7,20 @@ const newsSourceCheckboxesAndArticleAmounts = (sourceName, checked, onCheckedCha
         const validatedValue = Math.min(newValue, maxNumber);
         onNumArticlesChange(validatedValue);
     };
+
+    const increaseArticles = () => {
+        const newValue = numArticles + 1;
+        const maxNumber = 9; // You can adjust this value as needed
+        const validatedValue = Math.min(newValue, maxNumber);
+        onNumArticlesChange(validatedValue);
+    };
+    
+    const decreaseArticles = () => {
+        const newValue = numArticles - 1;
+        const minNumber = 1; // You can adjust this value as needed
+        const validatedValue = Math.max(newValue, minNumber);
+        onNumArticlesChange(validatedValue);
+    };
     
     return (
         <label className='source-label' htmlFor={sourceName}>
@@ -17,14 +31,18 @@ const newsSourceCheckboxesAndArticleAmounts = (sourceName, checked, onCheckedCha
                 onChange={(e) => onCheckedChange(e.target.checked)}
             />
             {sourceName}
-            {checked && ( 
-                <input
-                    type="number"
-                    className='amount-of-articles-input'
-                    id={`${sourceName}Articles`}
-                    value={numArticles}
-                    onChange={(e) => handleNumArticlesChange(Number(e.target.value))}
-                />
+            {checked && (
+                <>
+                    <button className='plus-button' onClick={increaseArticles}>+</button>
+                    <button className='minus-button' onClick={decreaseArticles}>-</button>
+                    <input
+                        type="number"
+                        className='amount-of-articles-input'
+                        id={`${sourceName}Articles`}
+                        value={numArticles}
+                        onChange={(e) => handleNumArticlesChange(Number(e.target.value))}
+                    />
+                </>
             )}
             <hr />
         </label>
